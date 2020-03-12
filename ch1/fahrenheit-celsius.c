f68550af5ca71eccd0c32d3059c2424aa6c72819
John Donnellan <john@codybartfast.com>
2020-03-12 12:21:35 +0000

    F2C using Symbols

--- ch1/fahrenheit-celsius.c
+++ ch1/fahrenheit-celsius.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
 
+#define   LOWER  0       /* lower limit of temperature */
+#define   UPPER  300     /* upper limit */
+#define   STEP   20      /* step size */
+
 /* print Fahrenhiet-Celsius table
     for fahr - 0, 20, ..., 300 */
 int main()
 {
-    float fahr, celsius;
-    int lower, upper, step;
+    int fahr;
 
-    lower = 0;      /* lower limit of temperature */
-    upper = 300;    /* upper limit */
-    step = 20;      /* step size */
+    printf("Temperature conversion from degress\n");
+    printf("Fahrenheit to degrees Celsius\n\n");
+    printf("   °F     °C\n");
+    printf("=====  =====\n");
 
-    fahr = lower;
-    while (fahr <= upper) {
-        celsius = (5.0/9.0) * (fahr-32.0);
-        printf("%3.0f %6.1f\n", fahr, celsius);
-        fahr = fahr + step;
-    }
+    for(fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
+        printf("%4d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+
+    printf("\n");
 }

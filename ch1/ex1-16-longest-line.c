d172850aba8826d89df95956d59bb610e46a258e
John Donnellan <john@codybartfast.com>
2020-03-13 20:01:34 +0000

    Ex 1-16 Longest line

--- ch1/ex1-16-longest-line.c
+++ ch1/ex1-16-longest-line.c
@@ -1,10 +1,13 @@
-/* Exercise 1-16. Revise the main routine of the longest-line program so it
- * will correctly print the length of arbitrarily long input lines, and as
- * much as possible of the text.
+/* Exercise 1-16. 
+
+Revise the main routine of the longest-line program so it  will correctly 
+print the length of arbitrarily long input lines, and as much as possible of
+the text.
+
  */
 
 #include <stdio.h>
-#define MAXLINE 1000    /* maximum input line size */
+#define MAXLINE 40      /* maximum input line size */
 
 int get_line(char line[], int maxline);
 void copy(char to[], char from[]);
@@ -12,10 +15,10 @@ void copy(char to[], char from[]);
 /* print longest input line */
 int main()
 {
-    int len;    /* current line length */
-    int max;    /* maximum length so far */
-    char line[MAXLINE]; /* current input line */
-    char longest[MAXLINE]; /* longest line saved here */
+    int len;                /* current line length */
+    int max;                /* maximum length so far */
+    char line[MAXLINE];     /* current input line */
+    char longest[MAXLINE];  /* longest line saved here */
 
     max = 0;
     while ((len = get_line(line, MAXLINE)) > 0)
@@ -24,7 +27,7 @@ int main()
             copy(longest, line);
         }
     if (max > 0)    /* there was a line */
-        printf("%s\n", longest);
+        printf("%d:%s\n", max, longest);
     return 0;
 }
 
@@ -32,14 +35,18 @@ int main()
 int get_line(char s[], int lim)
 {
     int c, i;
-
-    for (i=0; i<lim-1 && (c=getchar())!=EOF && c!='\n'; ++i)
-        s[i] = c;
+    for (i=0; (c=getchar())!=EOF && c!='\n'; ++i)
+        if (i<lim-1)
+            s[i] = c;
     if (c == '\n') {
-        s[i] = '\0';
+        if (i<lim-1)
+            s[i] = c;
         ++i;
     }
-    s[i] = '\0';
+    if (i < lim)
+        s[i] = '\0';   
+    else 
+        s[lim-1] = '\0';
     return i;
 }
 
@@ -51,3 +58,8 @@ void copy(char to[], char from[])
     while ((to[i] = from[i]) != '\0')
         ++i;
 }
+
+/*
+ *  cat lorem-ipsum.txt | a.out
+79:Lorem  ipsum  dolor sit amet,  consec
+ */

9289ffc8e94a52e47873c9fcec486d5df6fe8103
John Donnellan <john@codybartfast.com>
2020-04-14 10:44:00 +0100

    Ex 4-4 Stack commands

---
+++ ch4/ex-4-04-stack-commands.c
@@ -0,0 +1,205 @@
+/* Exercise 4-4
+ *
+ * Add commands to print the top element of the stack without poping, to
+ * duplicate it, and to swap the top two elements.  Add a command to clear
+ * the stack.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <ctype.h>
+
+#define MAXOP 100
+#define NUMBER '0'
+
+#define MAXVAL 100
+
+#define BUFSIZE 100
+
+int getop(char[]);
+void push(double);
+double pop(void);
+void peek(void);
+void duplicate(void);
+void swap(void);
+void clear(void);
+
+int sp = 0;
+double val[MAXVAL];
+
+int getch(void);
+void ungetch(int);
+
+char buf[BUFSIZE];
+int bufp = 0;
+
+int main(void)
+{
+	int type;
+	double op2;
+	char s[MAXOP];
+
+	while ((type = getop(s)) != EOF) {
+		switch (type) {
+		case NUMBER:
+			push(atof(s));
+			break;
+		case '+':
+			push(pop() + pop());
+			break;
+		case '*':
+			push(pop() * pop());
+			break;
+		case '-':
+			op2 = pop();
+			push(pop() - op2);
+			break;
+		case '/':
+			op2 = pop();
+			if (op2 != 0.0)
+				push(pop() / op2);
+			else
+				printf("error: zero divisor\n");
+			break;
+		case '%':
+			op2 = pop();
+			if (op2 != 0.0)
+				push((long)pop() % (long)op2);
+			else
+				printf("error: zero divisor (mod)\n");
+			break;
+		case '?':
+			peek();
+			break;
+		case '"':
+			duplicate();
+			break;
+		case '~':
+			swap();
+			break;
+		case '`':
+			clear();
+			break;
+		case '\n':
+			printf("\t%.8g\n", pop());
+			break;
+		default:
+			printf("error: unknown command %s\n", s);
+			break;
+		}
+	}
+	return 0;
+}
+
+void push(double f)
+{
+	if (sp < MAXVAL)
+		val[sp++] = f;
+	else
+		printf("error: stack full, can't push %g\n", f);
+}
+
+double pop(void)
+{
+	if (sp > 0)
+		return val[--sp];
+	else {
+		printf("error: stack empty\n");
+		return 0.0;
+	}
+}
+
+void peek(void)
+{
+	if (sp > 0)
+		printf("peek: %g\n", val[sp - 1']);
+	else
+		printf("error: stack empty\n");
+}
+
+void duplicate(void)
+{
+	if (sp <= 0)
+		printf("error: stack empty\n");
+	else if (sp >= MAXVAL)
+		printf("error: stack full, can't duplicate\n");
+	else
+		push(val[sp - 1]);
+}
+
+void swap(void)
+{
+	int t;
+	if (sp <= 1) {
+		printf("error: stack has fewer than two items\n");
+	} else {
+		t = val[sp - 2];
+		val[sp - 2] = val[sp - 1];
+		val[sp - 1] = t;
+	}
+}
+
+void clear(void)
+{
+	sp = 0;
+}
+
+int getop(char s[])
+{
+	int i, c, next;
+
+	while ((s[0] = c = getch()) == ' ' || c == '\t')
+		;
+	s[1] = '\0';
+	if (!isdigit(c) && c != '.' && c != '-')
+		return c;
+	if (c == '-') {
+		ungetch(next = getch());
+		if (!isdigit(next) && next != '.')
+			return c;
+	}
+	i = 0;
+	if (isdigit(c) || c == '-')
+		while (isdigit(s[++i] = c = getch()))
+			;
+	if (c == '.')
+		while (isdigit(s[++i] = c = getch()))
+			;
+	s[i] = '\0';
+	if (c != EOF)
+		ungetch(c);
+	return NUMBER;
+}
+
+int getch(void)
+{
+	return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+void ungetch(int c)
+{
+	if (bufp >= BUFSIZE)
+		printf("ungetch: too many characters\n");
+	else
+		buf[bufp++] = c;
+}
+
+/* Sample
+
+1 2 - ? 4 5 + *
+peek: -1
+    -9
+
+5 " *
+    25
+
+3 10 ~ -
+    7
+
+1 1 ` + 1 1 +
+error: stack empty
+error: stack empty
+    2
+
+*/

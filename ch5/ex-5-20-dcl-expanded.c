02241c763d11546f826a5929ee5ed9f56ca09226
John Donnellan <john@codybartfast.com>
2020-05-17 16:36:34 +0100

    5-20 named args

--- ch5/ex-5-20-dcl-expanded.c
+++ ch5/ex-5-20-dcl-expanded.c
@@ -5,28 +5,31 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
 
 #define MAXTOKEN 100
-#define BUFSIZE (1 << 10)
+#define SYMSIZE (1 << 10)
+#define MSGSIZE (1 << 10)
+#define BUFSIZE (1 << 1)
 
-enum { VAR, TYPE, BRACKETS };
+enum { TYPE, VAR, BRACKETS };
 enum { OK = 0, ERROR };
 enum { NO = 0, YES };
 
 int declaration(int reqname);
 
-int dcl(int reqname);
-int dirdcl(int reqname);
+int dcl(char *name, char *out, int reqname);
+int dirdcl(char *name, char *out, int reqname);
 
 void nextline(void);
 int gettoken(void);
 int tokentype;
-char token[MAXTOKEN];
-char name[MAXTOKEN];
-char datatype[MAXTOKEN];
-char out[1 << 10];
+char token[SYMSIZE];
+/* char name[SYMSIZE]; */
+/* char out[MSGSIZE]; */
+char dec[MSGSIZE];
 
 char printc(char c);
 int getch(void);
@@ -37,7 +40,7 @@ int bufp = 0;
 char *types[] = { "void",  "char",   "short",  "int",	  "long",
 		  "float", "double", "signed", "unsigned" };
 int ntypes = 9;
-int params(void);
+int params(char *out);
 int ws(void);
 int parseid(char *p);
 int brackets(char *p);
@@ -46,54 +49,62 @@ int cparens(void);
 
 int main(void)
 {
-	while (gettoken() != EOF)
+	while (gettoken() != EOF) {
+		/* out[0] = '\0'; */
 		if (declaration(YES) != OK) {
 			nextline();
 			return 0; /* for testing */
 		}
+		printf("\nTADA: %s\n\n", dec);
+	}
 	return 0;
 }
 
 int declaration(int reqname)
 {
+	char *datatype;
+	char name[SYMSIZE];
+	char out[MSGSIZE];
+
+	out[0] = '\0';
+
 	if (tokentype != TYPE) {
 		printf("\nerror: Expected a type\n");
 		return ERROR;
 	}
+	datatype = malloc((strlen(token) + 1) * sizeof(char));
 	strcpy(datatype, token);
-	out[0] = '\0';
-	if (dcl(reqname) != OK) {
+	if (dcl(name, out, reqname) != OK) {
 		return ERROR;
-	} else if (tokentype != ';') {
-		printf("\nsytax error\n");
+	} else if (tokentype != ';' && tokentype != ',' && tokentype != ')') {
+		printf("\nsytax error, got %d/%c\n", tokentype, tokentype);
 		return ERROR;
 	} else {
-		printf("\n");
-		printf("%s:%s %s\n\n", name, out, datatype);
+		sprintf(dec, "%s:%s %s", name, out, datatype);
 	}
 	return OK;
 }
 
-int dcl(int reqname)
+int dcl(char *name, char *out, int reqname)
 {
 	int ns, rslt;
 
 	for (ns = 0; gettoken() == '*';)
 		ns++;
-	if ((rslt = dirdcl(reqname)) != OK)
+	if ((rslt = dirdcl(name, out, reqname)) != OK)
 		return rslt;
 	while (ns-- > 0)
 		strcat(out, " pointer to");
 	return OK;
 }
 
-int dirdcl(int reqname)
+int dirdcl(char *name, char *out, int reqname)
 {
 	int rslt;
 	int gotnext = NO;
 
 	if (tokentype == '(') {
-		if ((rslt = dcl(reqname)) != OK)
+		if ((rslt = dcl(name, out, reqname)) != OK)
 			return rslt;
 		if (tokentype != ')') {
 			printf("\nerror: missing )\n");
@@ -102,6 +113,7 @@ int dirdcl(int reqname)
 	} else if (tokentype == VAR)
 		strcpy(name, token);
 	else if (!reqname) {
+		printf("\n-- implicit name -- (%d/%c)\n", tokentype, tokentype);
 		reqname = YES;
 		name[0] = '\0';
 		gotnext = YES;
@@ -114,9 +126,10 @@ int dirdcl(int reqname)
 	while (tokentype == '(' || tokentype == BRACKETS) {
 		if (tokentype == '(') {
 			strcat(out, " function taking");
-			if ((rslt = params()) != 0) {
+			if ((rslt = params(out)) != 0) {
 				return rslt;
 			}
+			/* printf("\nback from params with %s\n", out); */
 		} else {
 			strcat(out, " array");
 			strcat(out, token);
@@ -127,21 +140,36 @@ int dirdcl(int reqname)
 	return OK;
 }
 
-int params(void)
+int params(char *out)
 {
 	int argcount = 0;
+	char *seperator = " argument ";
+	/* printf("\nparams, out: %s\n", out); */
+
 	if (gettoken() != ')') {
 		do {
 			argcount++;
-		} while (gettoken() == ',');
+			if (tokentype == ',')
+				gettoken();
+			if (declaration(NO) != OK)
+				return ERROR;
+			/* printf("\nadding dec: %s\n", dec); */
+			strcat(out, seperator);
+			strcat(out, dec);
+			seperator = " and argument ";
+		} while (tokentype == ',');
+		/* printf("\nargcount %d, blah = %d/%c", argcount, tokentype,
+		       tokentype); */
 	}
-	if (argcount == 0)
-		strcat(out, " no arguments");
-	if (tokentype == ')')
+
+	if (tokentype == ')') {
+		if (argcount == 0)
+			strcat(out, " no arguments");
 		strcat(out, " and returning");
-	else {
+	} else {
 		printf("\nerror: expected closing parentheses "
-		       "after paramaters\n");
+		       "after paramaters (got %d/%c)\n",
+		       tokentype, tokentype);
 		return ERROR;
 	}
 	return OK;
@@ -155,6 +183,7 @@ int gettoken(void)
 
 	if (!(oparens() || brackets(p) || parseid(p)))
 		tokentype = getch();
+	/* printf("returning tkntype: %d/%c\n", tokentype, tokentype); */
 	return tokentype;
 }
 
@@ -243,8 +272,8 @@ void nextline(void)
 
 char printc(char c)
 {
-	if (c != EOF && c != '\n')
-		printf("%c", c);
+/* 	if (c != EOF && c != '\n')
+		printf("%c", c); */
 	return c;
 }
 

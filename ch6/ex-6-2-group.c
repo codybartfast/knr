ca7d7e375c109bba48e4cb98f48dd5e11a0fa945
John Donnellan <john@codybartfast.com>
2020-06-23 09:02:09 +0100

    6-2 check malloc

--- ch6/ex-6-2-group.c
+++ ch6/ex-6-2-group.c
@@ -1,11 +1,16 @@
 /*
  * Exercise 6-2
- * 
+ *
  * Write a program that reads a C program and prints in alphabetical order each
- * group of variable names that are identical in the first 6 characters, but 
- * different somewhere thereafter.  Don't count words within strings and 
+ * group of variable names that are identical in the first 6 characters, but
+ * different somewhere thereafter.  Don't count words within strings and
  * comments.  Make 6 a parameter that can be set from the command line.
  */
+
+// check malloc
+// pass input
+// ignore keywords
+// handle *vaname
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -45,15 +50,20 @@ int main(void)
 
 	ktree = NULL;
 	while (getword(word, MAXWORD) != EOF)
-		if (asalpha(word[0]))
+		if (asalpha(word[0])) {
 			ktree = addkey(ktree, getkey(word), word);
+			if (ktree == NULL) {
+				printf("error: Out of memory!\n");
+				return 1;
+			}
+		}
 	kprint(ktree);
 	return 0;
 }
 
 char *getkey(char *word)
 {
- 	int i;
+	int i;
 	char *k = key;
 	for (i = 0; i < keylen && *word; i++, k++, word++)
 		*k = *word;
@@ -65,16 +75,22 @@ struct knode *addkey(struct knode *p, char *k, char *w)
 {
 	int cond;
 	if (p == NULL) {
-		p = kalloc();
+		if ((p = kalloc()) == NULL)
+			return NULL;
 		p->key = strdup(k);
 		p->left = p->right = NULL;
-		p->wtree = addword(NULL, w);
+		if ((p->wtree = addword(NULL, w)) == NULL)
+			return NULL;
 	} else if ((cond = strcmp(k, p->key)) == 0) {
-		p->wtree = addword(p->wtree, w);
-	} else if (cond < 0)
-		p->left = addkey(p->left, k, w);
-	else
-		p->right = addkey(p->right, k, w);
+		if ((p->wtree = addword(p->wtree, w)) == NULL)
+			return NULL;
+	} else if (cond < 0) {
+		if ((p->left = addkey(p->left, k, w)) == NULL)
+			return NULL;
+	} else {
+		if ((p->right = addkey(p->right, k, w)) == NULL)
+			return NULL;
+	}
 	return p;
 }
 
@@ -97,14 +113,18 @@ struct wnode *addword(struct wnode *p, char *w)
 {
 	int cond;
 	if (p == NULL) {
-		p = walloc();
+		if ((p = walloc()) == NULL)
+			return NULL;
 		p->word = strdup(w);
 		p->left = p->right = NULL;
 	} else if ((cond = strcmp(w, p->word)) == 0) {
-	} else if (cond < 0)
-		p->left = addword(p->left, w);
-	else
-		p->right = addword(p->right, w);
+	} else if (cond < 0) {
+		if ((p->left = addword(p->left, w)) == NULL)
+			return NULL;
+	} else {
+		if ((p->right = addword(p->right, w)) == NULL)
+			return NULL;
+	}
 	return p;
 }
 

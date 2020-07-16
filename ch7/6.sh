153d5df6356124ffd7099103312947ea63baa6c6
John Donnellan <john@codybartfast.com>
2020-07-16 13:00:17 +0100

    7-6 compare with checks

---
+++ ch7/6.sh
@@ -0,0 +1,2 @@
+cc ex-7-6-comp.c && a.out comp1.txt comp2.txt
+
--- ch7/ex-7-6-comp-basic.c
+++ ch7/ex-7-6-comp-basic.c
@@ -1,7 +1,7 @@
 /*
  * Exercise 7-6.
- * 
- * Write a program to compare two files, printing the first line where they 
+ *
+ * Write a program to compare two files, printing the first line where they
  * differ.
  */
 

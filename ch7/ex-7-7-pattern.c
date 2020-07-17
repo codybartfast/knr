877ef611876725efd6e42722c167cf7abbb56b73
John Donnellan <john@codybartfast.com>
2020-07-17 10:46:48 +0100

    7-7 use chkfgets

--- ch7/ex-7-7-pattern.c
+++ ch7/ex-7-7-pattern.c
@@ -1,23 +1,33 @@
 /*
  * Exercise 7-7.
- * 
+ *
  * Modify the pattern finding program of Chapter 5 to take its input from a set
- * of named files or, if no files are named as arguments, from the standard 
+ * of named files or, if no files are named as arguments, from the standard
  * input.  Should the file name be printed when a matching line is found?
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #define MAXLINE 1000
 
-int get_line(char *line, int max);
+enum errors { NO_ERROR = 0, FILE_OPEN_ERROR, FILE_READ_ERROR };
+
+static char *chkfgets(char *s, int n, FILE *iop, char *path);
+static void close(void);
+
+char *inpath;
+FILE *infile;
+static char errormsg[MAXLINE];
 
 int main(int argc, char *argv[])
 {
 	char line[MAXLINE];
 	long lineno = 0;
-	int c, except = 0, number = 0, found = 0;
+	int c, except = 0, number = 0;
+	int found = 0; /* not returned */
+	infile = stdin;
 
 	while (--argc > 0 && (*++argv)[0] == '-') {
 		while ((c = *++argv[0])) {
@@ -39,7 +49,7 @@ int main(int argc, char *argv[])
 	if (argc != 1) {
 		printf("Usage: find -x -n pattern\n");
 	} else {
-		while (get_line(line, MAXLINE) > 0) {
+		while ((chkfgets(line, MAXLINE, infile, inpath)) != NULL) {
 			lineno++;
 			if ((strstr(line, *argv) != NULL) != except) {
 				if (number)
@@ -49,23 +59,34 @@ int main(int argc, char *argv[])
 			}
 		}
 	}
-	return found;
+	close();
+	exit(NO_ERROR);
 }
 
-int get_line(char s[], int lim)
+FILE *chkfopen(char *path, char *modes)
 {
-	int c, i;
-	for (i = 0; (c = getchar()) != EOF && c != '\n'; ++i)
-		if (i < lim - 1)
-			s[i] = c;
-	if (c == '\n') {
-		if (i < lim - 1)
-			s[i] = c;
-		++i;
-	}
-	if (i < lim)
-		s[i] = '\0';
-	else
-		s[lim - 1] = '\0';
-	return i;
+	FILE *fp = fopen(path, modes);
+	if (fp != NULL)
+		return fp;
+	sprintf(errormsg, "error: Failed to open file: '%s'", path);
+	perror(errormsg);
+	close();
+	exit(FILE_OPEN_ERROR);
+}
+
+char *chkfgets(char *s, int n, FILE *iop, char *path)
+{
+	char *r = fgets(s, n, iop);
+	if (!ferror(iop))
+		return r;
+	sprintf(errormsg, "error: Failed to read file: '%s'", path);
+	perror(errormsg);
+	close();
+	exit(FILE_READ_ERROR);
+}
+
+void close(void)
+{
+	if (infile != NULL && infile != stdin)
+		fclose(infile);
 }

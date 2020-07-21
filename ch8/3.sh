86f50f73d0f9b6f59555cffaf5903f48213c2468
John Donnellan <john@codybartfast.com>
2020-07-21 18:40:14 +0100

    8-3 flush

---
+++ ch8/3.sh
@@ -0,0 +1,4 @@
+cc ex-8-3-flush.c && a.out ex-8-3-flush.c out.txt
+cat out.txt
+rm out.txt
+
---
+++ ch8/ex-8-3-flush.c
@@ -0,0 +1,225 @@
+/*
+ * Exercise 8-3.
+ *
+ * Design and write _flushbuf, fflush and fclose.
+ */
+
+/**********
+ *  FILE  *
+ **********/
+
+#define NULL 0
+#define EOF (-1)
+#define OPEN_MAX 20
+
+typedef struct _iobuf {
+	int cnt;
+	char *ptr;
+	char *base;
+	int flag;
+	int fd;
+} FILE;
+extern FILE _iob[OPEN_MAX];
+
+#define stdin (&_iob[0])
+#define stdout (&_iob[1])
+#define stderr (&_iob[2])
+
+enum flags { _READ = 01, _WRITE = 02, _UNBUF = 04, _EOF = 010, _ERR = 020 };
+
+FILE *fopen(char *name, char *mode);
+int _fillbuf(FILE *);
+int _flushbuf(int, FILE *);
+int fflush(FILE *);
+int fclose(FILE *);
+
+#define feof(p) (((p)->flag & _EOF) != 0)
+#define ferror(p) (((p)->flag & _ERR) != 0)
+#define fileno(p) ((p)->fd)
+
+#define getc(p) (--(p)->cnt >= 0 ? (unsigned char)*(p)->ptr++ : _fillbuf(p))
+#define putc(x, p) (--(p)->cnt >= 0 ? *(p)->ptr++ = (x) : _flushbuf((x), p))
+
+#define getchar() getc(stdin)
+#define putchar(x) putc(x, stdout)
+
+/**************************
+ *  stdin, stdou, stderr  *
+ **************************/
+#define BUFSIZE 1024
+
+char stdinbuf[BUFSIZE];
+char stdoutbuf[BUFSIZE];
+char stderrbuf[BUFSIZE];
+
+FILE _iob[OPEN_MAX] = { { 0, (char *)0, stdinbuf, _READ, 0 },
+			{ 0, (char *)0, stdoutbuf, _WRITE, 1 },
+			{ 0, (char *)0, stderrbuf, _WRITE | _UNBUF, 2 } };
+
+/***********
+ *  fopen  *
+ ***********/
+
+#include <fcntl.h>
+#include <unistd.h>
+#define PERMS 0666
+
+FILE *fopen(char *name, char *mode)
+{
+	int fd;
+	FILE *fp;
+
+	if (*mode != 'r' && *mode != 'w' && *mode != 'a')
+		return NULL;
+	for (fp = _iob; fp < _iob + OPEN_MAX; fp++)
+		if ((fp->flag & (_READ | _WRITE)) == 0)
+			break;
+	if (fp >= _iob + OPEN_MAX)
+		return NULL;
+
+	if (*mode == 'w')
+		fd = creat(name, PERMS);
+	else if (*mode == 'a') {
+		if ((fd = open(name, O_WRONLY, 0)) == -1)
+			fd = creat(name, PERMS);
+		lseek(fd, 0L, 2);
+	} else
+		fd = open(name, O_RDONLY, 0);
+	if (fd == -1)
+		return NULL;
+	fp->fd = fd;
+	fp->cnt = 0;
+	fp->base = NULL;
+	fp->flag = (*mode == 'r') ? _READ : _WRITE;
+	return fp;
+}
+
+/**************
+ *  _fillbuf  *
+ **************/
+
+#include <stdlib.h>
+
+int _fillbuf(FILE *fp)
+{
+	int bufsize;
+
+	if ((fp->flag & (_READ | _EOF | _ERR)) != _READ)
+		return EOF;
+	bufsize = (fp->flag & _UNBUF) ? 1 : BUFSIZE;
+	if (fp->base == NULL)
+		if ((fp->base = (char *)malloc(bufsize)) == NULL)
+			return EOF;
+	fp->ptr = fp->base;
+	fp->cnt = read(fp->fd, fp->ptr, bufsize);
+	if (--fp->cnt < 0) {
+		if (fp->cnt == -1)
+			fp->flag |= _EOF;
+		if (fp->cnt == -2)
+			fp->flag |= _ERR;
+		fp->cnt = 0;
+		return EOF;
+	}
+	return (unsigned char)*fp->ptr++;
+}
+
+/**************
+ *  _fillbuf  *
+ **************/
+
+int _flushbuf(int c, FILE *fp)
+{
+	if ((fp->flag & (_WRITE | _EOF | _ERR)) != _WRITE)
+		return EOF;
+	if (fp->base == NULL) {
+		int bufsize = (fp->flag & _UNBUF) ? 1 : BUFSIZE;
+		if ((fp->base = (char *)malloc(bufsize)) == NULL)
+			return EOF;
+		fp->ptr = fp->base;
+		fp->cnt = bufsize;
+	} else if (fflush(fp))
+		return EOF;
+	*fp->ptr++ = c;
+	fp->cnt--;
+	return 0;
+}
+
+/************
+ *  fflush  *
+ ************/
+
+int fflush(FILE *fp)
+{
+	int bufsize;
+
+	if (fp == NULL) {
+		int i, rslt = 0;
+		for (i = 0; i < OPEN_MAX; i++)
+			if ((fp = &_iob[i])->flag & (_READ | _WRITE))
+				rslt |= fflush(fp);
+		return rslt;
+	}
+
+	if ((fp->flag & (_WRITE | _EOF | _ERR)) != _WRITE)
+		return EOF;
+	if (fp->base == NULL)
+		return EOF;
+	bufsize = (fp->flag & _UNBUF) ? 1 : BUFSIZE;
+	if (write(fp->fd, fp->base, bufsize - fp->cnt) < 0) {
+		fp->flag |= _ERR;
+		return EOF;
+	}
+	fp->cnt = bufsize;
+	fp->ptr = fp->base;
+	return 0;
+}
+
+/************
+ *  fclose  *
+ ************/
+
+int fclose(FILE *fp)
+{
+	if (fp == NULL)
+		return EOF;
+	if (fp->flag & _WRITE)
+		if (fflush(fp))
+			return EOF;
+	if (fp->fd <= 2)
+		return 0;
+	fp->flag = 0; /* mark as unused */
+	free(fp->base);
+	fp->base = NULL;
+	return close(fp->fd);
+}
+
+/*****************
+ *  main (copy)  *
+ *****************/
+
+int main(int argc, char *argv[])
+{
+	FILE *from, *to;
+	char c;
+
+	if (argc != 3) {
+		write(2, &"error: Expect 3 args!\n", 22);
+		return 1;
+	}
+	if ((from = fopen(argv[1], "r")) == NULL) {
+		write(2, &"error: failed to open source!\n", 30);
+		return 1;
+	}
+	if ((to = fopen(argv[2], "w")) == NULL) {
+		write(2, &"error: failed to open target!\n", 30);
+		return 1;
+	}
+
+	while ((c = getc(from)) != EOF)
+		putchar(c);
+
+	fflush(NULL);
+	fclose(from);
+	fclose(to);
+	return 0;
+}

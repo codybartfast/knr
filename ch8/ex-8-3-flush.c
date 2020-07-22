95ffa2320a175f43a842c82f6cbac08fccc2a26d
John Donnellan <john@codybartfast.com>
2020-07-22 09:43:07 +0100

    8-3 fflush - more tweaking

--- ch8/ex-8-3-flush.c
+++ ch8/ex-8-3-flush.c
@@ -5,6 +5,7 @@
  */
 
 #include <fcntl.h>
+#include <stdlib.h>
 #include <unistd.h>
 
 #undef NULL
@@ -18,6 +19,7 @@ typedef struct _iobuf {
 	char *base;
 	int flag;
 	int fd;
+	int bufsize;
 } FILE;
 
 enum flags { _READ = 01, _WRITE = 02, _UNBUF = 04, _EOF = 010, _ERR = 020 };
@@ -25,6 +27,9 @@ enum flags { _READ = 01, _WRITE = 02, _UNBUF = 04, _EOF = 010, _ERR = 020 };
 #define feof(p) (((p)->flag & _EOF) != 0)
 #define ferror(p) (((p)->flag & _ERR) != 0)
 #define fileno(p) ((p)->fd)
+#define isopen(p) ((p)->flag & (_READ | _WRITE))
+#define markclosed(p) ((p)->flag &= ~(_READ | _WRITE))
+#define notreadyfor(p, rd_wr) (((p)->flag & (rd_wr | _EOF | _ERR)) != rd_wr)
 
 #define getc(p) (--(p)->cnt >= 0 ? (unsigned char)*(p)->ptr++ : _fillbuf(p))
 #define putc(x, p) (--(p)->cnt >= 0 ? *(p)->ptr++ = (x) : _flushbuf((x), p))
@@ -32,21 +37,80 @@ enum flags { _READ = 01, _WRITE = 02, _UNBUF = 04, _EOF = 010, _ERR = 020 };
 #define getchar() getc(stdin)
 #define putchar(x) putc(x, stdout)
 
+FILE _iob[OPEN_MAX] = { { 0, (char *)0, (char *)0, _READ, 0, 0 },
+			{ 0, (char *)0, (char *)0, _WRITE, 1, 0 },
+			{ 0, (char *)0, (char *)0, _WRITE | _UNBUF, 2, 0 } };
+
 #define stdin (&_iob[0])
 #define stdout (&_iob[1])
 #define stderr (&_iob[2])
 
-FILE _iob[OPEN_MAX] = { { 0, (char *)0, (char *)0, _READ, 0 },
-			{ 0, (char *)0, (char *)0, _WRITE, 1 },
-			{ 0, (char *)0, (char *)0, _WRITE | _UNBUF, 2 } };
-
 FILE *fopen(char *name, char *mode);
-int _fillbuf(FILE *);
-int _flushbuf(int, FILE *);
 int fflush(FILE *);
 int fclose(FILE *);
+static int _fillbuf(FILE *);
+static int _flushbuf(int, FILE *);
 
+#define BUFSIZE 1024
 #define PERMS 0666
+
+int fflush(FILE *fp)
+{
+	int n;
+
+	if (fp == NULL) {
+		int i, rslt = 0;
+		for (i = 0; i < OPEN_MAX; i++)
+			if ((fp = &_iob[i])->flag & _WRITE)
+				rslt |= fflush(fp);
+		return rslt;
+	}
+
+	if (notreadyfor(fp, _WRITE))
+		return EOF;
+	if (fp->base == NULL)
+		return 0;
+	if ((n = fp->bufsize - fp->cnt))
+		if (write(fp->fd, fp->base, n) != n) {
+			fp->flag |= _ERR;
+			return EOF;
+		}
+	fp->cnt = fp->bufsize;
+	fp->ptr = fp->base;
+	return 0;
+}
+
+int _flushbuf(int c, FILE *fp)
+{
+	if (notreadyfor(fp, _WRITE))
+		return EOF;
+	if (fp->base == NULL) {
+		fp->bufsize = (fp->flag & _UNBUF) ? 1 : BUFSIZE;
+		if ((fp->base = (char *)malloc(fp->bufsize)) == NULL)
+			return EOF;
+		fp->ptr = fp->base;
+		fp->cnt = fp->bufsize;
+	} else if (fflush(fp))
+		return EOF;
+	*fp->ptr++ = c;
+	fp->cnt--;
+	return 0;
+}
+
+int fclose(FILE *fp)
+{
+	int rslt = 0;
+	if (fp == NULL)
+		return EOF;
+	if (fp->flag & _WRITE)
+		rslt = fflush(fp);
+	free(fp->base);
+	fp->base = NULL;
+	if (fp->fd <= 2)
+		return rslt;
+	markclosed(fp);
+	return rslt | close(fp->fd);
+}
 
 FILE *fopen(char *name, char *mode)
 {
@@ -56,7 +120,7 @@ FILE *fopen(char *name, char *mode)
 	if (*mode != 'r' && *mode != 'w' && *mode != 'a')
 		return NULL;
 	for (fp = _iob; fp < _iob + OPEN_MAX; fp++)
-		if ((fp->flag & (_READ | _WRITE)) == 0)
+		if (!isopen(fp))
 			break;
 	if (fp >= _iob + OPEN_MAX)
 		return NULL;
@@ -74,25 +138,22 @@ FILE *fopen(char *name, char *mode)
 	fp->fd = fd;
 	fp->cnt = 0;
 	fp->base = NULL;
+	fp->bufsize = 0;
 	fp->flag = (*mode == 'r') ? _READ : _WRITE;
 	return fp;
 }
 
-#include <stdlib.h>
-#define BUFSIZE 1024
-
 int _fillbuf(FILE *fp)
 {
-	int bufsize;
-
-	if ((fp->flag & (_READ | _EOF | _ERR)) != _READ)
+	if (notreadyfor(fp, _READ))
 		return EOF;
-	bufsize = (fp->flag & _UNBUF) ? 1 : BUFSIZE;
-	if (fp->base == NULL)
-		if ((fp->base = (char *)malloc(bufsize)) == NULL)
+	if (fp->base == NULL) {
+		fp->bufsize = (fp->flag & _UNBUF) ? 1 : BUFSIZE;
+		if ((fp->base = (char *)malloc(fp->bufsize)) == NULL)
 			return EOF;
+	}
 	fp->ptr = fp->base;
-	fp->cnt = read(fp->fd, fp->ptr, bufsize);
+	fp->cnt = read(fp->fd, fp->ptr, fp->bufsize);
 	if (--fp->cnt < 0) {
 		if (fp->cnt == -1)
 			fp->flag |= _EOF;
@@ -102,65 +163,6 @@ int _fillbuf(FILE *fp)
 		return EOF;
 	}
 	return (unsigned char)*fp->ptr++;
-}
-
-int _flushbuf(int c, FILE *fp)
-{
-	if ((fp->flag & (_WRITE | _EOF | _ERR)) != _WRITE)
-		return EOF;
-	if (fp->base == NULL) {
-		int bufsize = (fp->flag & _UNBUF) ? 1 : BUFSIZE;
-		if ((fp->base = (char *)malloc(bufsize)) == NULL)
-			return EOF;
-		fp->ptr = fp->base;
-		fp->cnt = bufsize;
-	} else if (fflush(fp))
-		return EOF;
-	*fp->ptr++ = c;
-	fp->cnt--;
-	return 0;
-}
-
-int fflush(FILE *fp)
-{
-	int bufsize;
-
-	if (fp == NULL) {
-		/* flush all open streams */
-		int i, rslt = 0;
-		for (i = 0; i < OPEN_MAX; i++)
-			if ((fp = &_iob[i])->flag & (_READ | _WRITE))
-				rslt |= fflush(fp);
-		return rslt;
-	}
-
-	if ((fp->flag & (_WRITE | _EOF | _ERR)) != _WRITE)
-		return EOF;
-	if (fp->base == NULL)
-		return 0;
-	bufsize = (fp->flag & _UNBUF) ? 1 : BUFSIZE;
-	if (write(fp->fd, fp->base, bufsize - fp->cnt) < 0) {
-		fp->flag |= _ERR;
-		return EOF;
-	}
-	fp->cnt = bufsize;
-	fp->ptr = fp->base;
-	return 0;
-}
-
-int fclose(FILE *fp)
-{
-	if (fp == NULL)
-		return EOF;
-	if (fp->flag & _WRITE)
-		if (fflush(fp))
-			return EOF;
-	free(fp->base);
-	fp->base = NULL;
-	if (fp->fd <= 2)
-		return 0;
-	fp->flag &= ~(_READ | _WRITE); /* mark as unused */
-	return close(fp->fd);
 }
 
 int main(int argc, char *argv[])
@@ -179,6 +181,7 @@ int main(int argc, char *argv[])
 		write(2, &"error: failed to open source!\n", 30);
 		return 1;
 	}
+
 	if ((temp = fopen(tpath, "w")) == NULL) {
 		write(2, &"error: failed to open temp to write!\n", 37);
 		return 1;
